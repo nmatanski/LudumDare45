a2d4375c2a780c0c5a81a726f5cc027b2c73d473
dogiordano <dogiordano@gmail.com>
2019-10-05 18:19:16 +0200

    First commit after forking

---
+++ Assets/Scripts/Place.cs
@@ -0,0 +1,33 @@
+﻿namespace Stray
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public sealed class Place : MonoBehaviour
+    {
+        [SerializeField]
+        string m_Name;
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        [SerializeField]
+        string m_Description;
+        public string Description
+        {
+            get { return m_Description; }
+            set { m_Description = value; }
+        }
+
+        [SerializeField]
+        List<PlaceAction> m_Actions;
+        public List<PlaceAction> Actions
+        {
+            get { return m_Actions; }
+            set { m_Actions = value; }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlaceAction.cs
@@ -0,0 +1,17 @@
+﻿namespace Stray
+{
+    using UnityEngine;
+
+    public sealed class PlaceAction : MonoBehaviour
+    {
+        [SerializeField]
+        bool m_OneTime = true;
+        public bool OneTime
+        {
+            get { return m_OneTime; }
+            set { m_OneTime = value; }
+        }
+
+    }
+
+}
