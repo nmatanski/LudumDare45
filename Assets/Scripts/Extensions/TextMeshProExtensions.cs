492a144ee2953358123eea44c5a9c3348c4eb48b
nmatanski <nmatanski95@abv.bg>
2019-10-05 23:42:49 +0300

    Textbox typewriter effect added through an extension method

---
+++ Assets/Scripts/Extensions/TextMeshProExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Stray
+{
+    public static class TextMeshProExtensions
+    {
+        public static IEnumerator DisplayTextWithDelay(this TextMeshProUGUI textbox)
+        {
+            string text = textbox.text;
+            textbox.text = "";
+            foreach (var character in text)
+            {
+                textbox.text += character;
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+    }
+}
