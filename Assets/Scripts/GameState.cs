24a8ed3f14b52d7dc391ed603ced04c32ed201e1
nmatanski <nmatanski95@abv.bg>
2019-10-05 14:31:05 +0300

    Initial commit: empty project with only game state singleton class

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    [SerializeField]
+    private short time = 9;
+
+
+    private void Awake()
+    {
+        int sessionsCount = FindObjectsOfType<GameState>().Length;
+        if (sessionsCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        time++;
+    }
+}
