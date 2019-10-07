23ac01b1c2e9187c33c00042d3bd78b76adf88c3
nmatanski <nmatanski95@abv.bg>
2019-10-07 20:28:05 +0300

    SFXExtension for Audio Fade Out added

---
+++ Assets/Scripts/FX/Extensions/SFXExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Stray
+{
+    public static class SFXExtensions
+    {
+        public static IEnumerator FadeOutAudio(this AudioSource source, float duration)
+        {
+            float defaultVolume = source.volume;
+
+            while (source.volume > 0)
+            {
+                source.volume -= defaultVolume * Time.deltaTime / duration;
+                yield return new WaitForEndOfFrame();
+            }
+
+            source.Stop();
+            source.volume = defaultVolume;
+        }
+    }
+}
---
+++ Assets/Scripts/SoundFX.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Stray
+{
+    public class SoundFX : MonoBehaviour
+    {
+        [SerializeField]
+        private SoundFX nextSoundFX;
+
+
+        public void PlaySound(AudioClip clip)
+        {
+            AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+            //Destroy(gameObject);
+        }
+    }
+}
